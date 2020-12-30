028e9dd82e63c6f36c5de3797e20796022eb32b7
xukechen <xukechen@rsun.com>
2020-12-30 21:45:44 +0800

    init repository

---
+++ src/main/java/com/xkc/learn/Test.java
@@ -0,0 +1,14 @@
+package com.xkc.learn;
+
+import lombok.Data;
+
+@Data
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(
+                111
+        );
+    }
+
+}
